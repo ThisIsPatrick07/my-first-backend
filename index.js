91c1d0866eb050be799622bf18a0b960af215ddc
Prateik Lohani <lohaniprateik@gmail.com>
2024-06-26 19:03:41 +0530

    committed

---
+++ index.js
@@ -0,0 +1,22 @@
+require('dotenv').config();
+const express = require('express');
+const app = express();
+const port = 3000;
+
+// req is request
+// res is response
+app.get('/', (req, res) => {
+	res.send('Hello world!');
+});
+
+app.get('/twitter', (req, res) => {
+	res.send('niceboi.com');
+});
+
+app.get('/login', (req, res) => {
+	res.send('<h1>You have made a web app! Congrats!</h1>');
+});
+
+app.listen(process.env.PORT, () => {
+	console.log(`Example app listening on port ${port}`);
+});
